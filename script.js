574b436401b2f0745fafd26f90d601d77b6a09c9
= <=>
2022-02-16 13:44:46 +0600

    finished js

---
+++ script.js
@@ -0,0 +1,317 @@
+let countryCode = {
+   BDT: "BD",
+   EUR: "BE",
+   XOF: "BF",
+   BGN: "BG",
+   BAM: "BA",
+   BBD: "BB",
+   XPF: "WF",
+   EUR: "BL",
+   BMD: "BM",
+   BND: "BN",
+   BOB: "BO",
+   BHD: "BH",
+   BIF: "BI",
+   XOF: "BJ",
+   BTN: "BT",
+   JMD: "JM",
+   NOK: "BV",
+   BWP: "BW",
+   WST: "WS",
+   USD: "BQ",
+   BRL: "BR",
+   BSD: "BS",
+   GBP: "JE",
+   BYR: "BY",
+   BZD: "BZ",
+   RUB: "RU",
+   RWF: "RW",
+   RSD: "RS",
+   USD: "TL",
+   EUR: "RE",
+   TMT: "TM",
+   TJS: "TJ",
+   RON: "RO",
+   NZD: "TK",
+   XOF: "GW",
+   USD: "GU",
+   GTQ: "GT",
+   GBP: "GS",
+   EUR: "GR",
+   XAF: "GQ",
+   EUR: "GP",
+   JPY: "JP",
+   GYD: "GY",
+   GBP: "GG",
+   EUR: "GF",
+   GEL: "GE",
+   XCD: "GD",
+   GBP: "GB",
+   XAF: "GA",
+   USD: "SV",
+   GNF: "GN",
+   GMD: "GM",
+   DKK: "GL",
+   GIP: "GI",
+   GHS: "GH",
+   OMR: "OM",
+   TND: "TN",
+   JOD: "JO",
+   HRK: "HR",
+   HTG: "HT",
+   HUF: "HU",
+   HKD: "HK",
+   HNL: "HN",
+   AUD: "HM",
+   VEF: "VE",
+   USD: "PR",
+   ILS: "PS",
+   USD: "PW",
+   EUR: "PT",
+   NOK: "SJ",
+   PYG: "PY",
+   IQD: "IQ",
+   PAB: "PA",
+   XPF: "PF",
+   PGK: "PG",
+   PEN: "PE",
+   PKR: "PK",
+   PHP: "PH",
+   NZD: "PN",
+   PLN: "PL",
+   EUR: "PM",
+   ZMK: "ZM",
+   MAD: "EH",
+   EUR: "EE",
+   EGP: "EG",
+   ZAR: "ZA",
+   USD: "EC",
+   EUR: "IT",
+   VND: "VN",
+   SBD: "SB",
+   ETB: "ET",
+   SOS: "SO",
+   ZWL: "ZW",
+   SAR: "SA",
+   EUR: "ES",
+   ERN: "ER",
+   EUR: "ME",
+   MDL: "MD",
+   MGA: "MG",
+   EUR: "MF",
+   MAD: "MA",
+   EUR: "MC",
+   UZS: "UZ",
+   MMK: "MM",
+   XOF: "ML",
+   MOP: "MO",
+   MNT: "MN",
+   USD: "MH",
+   MKD: "MK",
+   MUR: "MU",
+   EUR: "MT",
+   MWK: "MW",
+   MVR: "MV",
+   EUR: "MQ",
+   USD: "MP",
+   XCD: "MS",
+   MRO: "MR",
+   GBP: "IM",
+   UGX: "UG",
+   TZS: "TZ",
+   MYR: "MY",
+   MXN: "MX",
+   ILS: "IL",
+   EUR: "FR",
+   USD: "IO",
+   SHP: "SH",
+   EUR: "FI",
+   FJD: "FJ",
+   FKP: "FK",
+   USD: "FM",
+   DKK: "FO",
+   NIO: "NI",
+   EUR: "NL",
+   NOK: "NO",
+   NAD: "NA",
+   VUV: "VU",
+   XPF: "NC",
+   XOF: "NE",
+   AUD: "NF",
+   NGN: "NG",
+   NZD: "NZ",
+   NPR: "NP",
+   AUD: "NR",
+   NZD: "NU",
+   NZD: "CK",
+   EUR: "XK",
+   XOF: "CI",
+   CHF: "CH",
+   COP: "CO",
+   CNY: "CN",
+   XAF: "CM",
+   CLP: "CL",
+   AUD: "CC",
+   CAD: "CA",
+   XAF: "CG",
+   XAF: "CF",
+   CDF: "CD",
+   CZK: "CZ",
+   EUR: "CY",
+   AUD: "CX",
+   CRC: "CR",
+   ANG: "CW",
+   CVE: "CV",
+   CUP: "CU",
+   SZL: "SZ",
+   SYP: "SY",
+   ANG: "SX",
+   KGS: "KG",
+   KES: "KE",
+   SSP: "SS",
+   SRD: "SR",
+   AUD: "KI",
+   KHR: "KH",
+   XCD: "KN",
+   KMF: "KM",
+   STD: "ST",
+   EUR: "SK",
+   KRW: "KR",
+   EUR: "SI",
+   KPW: "KP",
+   KWD: "KW",
+   XOF: "SN",
+   EUR: "SM",
+   SLL: "SL",
+   SCR: "SC",
+   KZT: "KZ",
+   KYD: "KY",
+   SGD: "SG",
+   SEK: "SE",
+   SDG: "SD",
+   DOP: "DO",
+   XCD: "DM",
+   DJF: "DJ",
+   DKK: "DK",
+   USD: "VG",
+   EUR: "DE",
+   YER: "YE",
+   DZD: "DZ",
+   USD: "US",
+   UYU: "UY",
+   EUR: "YT",
+   USD: "UM",
+   LBP: "LB",
+   XCD: "LC",
+   LAK: "LA",
+   AUD: "TV",
+   TWD: "TW",
+   TTD: "TT",
+   TRY: "TR",
+   LKR: "LK",
+   CHF: "LI",
+   EUR: "LV",
+   TOP: "TO",
+   LTL: "LT",
+   EUR: "LU",
+   LRD: "LR",
+   LSL: "LS",
+   THB: "TH",
+   EUR: "TF",
+   XOF: "TG",
+   XAF: "TD",
+   USD: "TC",
+   LYD: "LY",
+   EUR: "VA",
+   XCD: "VC",
+   AED: "AE",
+   EUR: "AD",
+   XCD: "AG",
+   AFN: "AF",
+   XCD: "AI",
+   USD: "VI",
+   ISK: "IS",
+   IRR: "IR",
+   AMD: "AM",
+   ALL: "AL",
+   AOA: "AO",
+   USD: "AS",
+   ARS: "AR",
+   AUD: "AU",
+   EUR: "AT",
+   AWG: "AW",
+   INR: "IN",
+   EUR: "AX",
+   AZN: "AZ",
+   EUR: "IE",
+   IDR: "ID",
+   UAH: "UA",
+   QAR: "QA",
+   MZN: "MZ"
+};
+
+const selectList = document.querySelectorAll(".selectGroup select"),
+   convertButton = document.getElementById("button"),
+   fromCurrency = document.querySelector(".from select"),
+   toCurrency = document.querySelector(".to select");
+
+for (let i = 0; i < selectList.length; i++) {
+   let selected = "";
+
+   for (currencyCode in countryCode) {
+      if (i == 0) {
+         selected = currencyCode == "USD" ? "selected" : "";
+      } else if (i == 1) {
+         selected = currencyCode == "BDT" ? "selected" : "";
+      }
+      let optionTag = `<option value="${currencyCode}" ${selected}>${currencyCode}</option>`;
+      selectList[i].insertAdjacentHTML("beforeend", optionTag);
+   }
+
+   window.addEventListener("load", (e) => {
+      getCurrentRate();
+   });
+   let checkClicked = 0;
+   convertButton.addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+      e.preventDefault();
+      checkClicked++;
+      getCurrentRate();
+   });
+
+   function getCurrentRate() {
+      const exchangeRateText = document.querySelector(".exchangeRate");
+      exchangeRateText.innerHTML = "Getting Rate....";
+      const amount = document.querySelector(".inputGroup input");
+      let amountValue = amount.value;
+
+      if (amountValue == "0" || amountValue == "") {
+         amount.value = "1";
+         amountValue = 1;
+      }
+
+      let apiKey = "b3bd12c6a6ef0181426d27bc";
+      let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`;
+
+      fetch(url)
+         .then((res) => res.json())
+         .then((data) => {
+            const conversionRate = data.conversion_rates[toCurrency.value];
+            let totalConversion = (amountValue * conversionRate).toFixed(2);
+            let conVal = "";
+            if (checkClicked !== 0) {
+               conVal =
+                  amountValue +
+                  fromCurrency.value +
+                  "=" +
+                  data.conversion_rates[toCurrency.value].toFixed(2) +
+                  toCurrency.value +
+                  "</br>";
+            }
+
+            exchangeRateText.innerHTML = `
+            1 ${fromCurrency.value} = ${totalConversion} ${toCurrency.value} </br>
+            <b>${conVal}</b>`;
+         });
+   }
+}
